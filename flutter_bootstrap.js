cb66962147b09f9cfaba4137a8f9383679cccbec
DESKTOP-VSD418Q\ACER <dlrlgk4506@naver.com>
2026-06-16 16:27:28 +0900

    버그패치

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2458717795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "206197562" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
